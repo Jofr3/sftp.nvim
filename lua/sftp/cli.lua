94989d50b12f05d4378e154287355d14054dfae6
Jofr3 <jofrescari@gmail.com>
2025-07-23 21:55:46 +0200

    testing rsync commands with spawn

--- lua/sftp/cli.lua
+++ lua/sftp/cli.lua
@@ -3,30 +3,48 @@ local ui = require("sftp.ui")
 
 local M = {}
 
-function M.check_connection()
-	local result = false
+function M.check_connection(callback)
+  -- rsync -avz ~/lsw/myclientum-new/test1.md myclientum_dev:/dev.myclientum.com/test.md
+	local cmd = "rsync"
+	local args = { "-avz", "~/lsw/myclientum-new/test1.md", "myclientum_dev:/dev.myclientum.com/test.md" }
 
 	local stdout_data = {}
 	local stderr_data = {}
 
-	local cmd = "ls"
-	local args = { "-la" }
+	local stdout = vim.loop.new_pipe(false)
+	local stderr = vim.loop.new_pipe(false)
 
 	local process
 	process = vim.loop.spawn(cmd, {
 		args = args,
-		stdio = { nil, 1, 2 },
+		stdio = { nil, stdout, stderr },
 	}, function(exit_code, signal)
-		if exit_code == 0 then
-			result = true
-		else
-			result = false
-		end
-
+		stdout:close()
+		stderr:close()
 		process:close()
+
+		local result = (exit_code == 0)
+		callback(result, {
+			exit_code = exit_code,
+			signal = signal,
+			stdout = table.concat(stdout_data),
+			stderr = table.concat(stderr_data),
+		})
 	end)
 
-	return result
+	stdout:read_start(function(err, data)
+		if err then
+		elseif data then
+			table.insert(stdout_data, data)
+		end
+	end)
+
+	stderr:read_start(function(err, data)
+		if err then
+		elseif data then
+			table.insert(stderr_data, data)
+		end
+	end)
 end
 
 return M
--- lua/sftp/sftp.lua
+++ lua/sftp/sftp.lua
@@ -12,14 +12,24 @@ function M.setup(opts)
 end
 
 function M.check_project()
-  connected = false
+	connected = false
 	local current_path = vim.fn.getcwd()
 
 	for key, project in pairs(plugin_opts) do
 		if project.local_path == current_path and project.host ~= nil and project.remote_path ~= nil then
-			print("in a sftp project")
 
-      -- connected = cli.check_connection()
+			cli.check_connection(function(success, details)
+        print(vim.inspect(details))
+        if not success then
+          print("Error connecting to server.")
+        else
+          print("ok")
+        end
+
+        -- error code 23 = file not found
+
+        -- check if actuall output of command means connection with sftp server is made
+			end)
 		end
 	end
 end
