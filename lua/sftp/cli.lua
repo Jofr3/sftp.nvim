107c8169b3cd23dae86cd5446561f6e635d44219
Jofr3 <jofrescari@gmail.com>
2025-07-24 15:01:17 +0200

    added on save autocmd and improved cli function

--- lua/sftp/cli.lua
+++ lua/sftp/cli.lua
@@ -3,26 +3,21 @@ local ui = require("sftp.ui")
 
 local M = {}
 
-function M.check_connection(callback)
-  -- rsync -avz ~/lsw/myclientum-new/test1.md myclientum_dev:/dev.myclientum.com/test.md
-	local cmd = "rsync"
-	local args = { "-avz", "~/lsw/myclientum-new/test1.md", "myclientum_dev:/dev.myclientum.com/test.md" }
-
+function M.execute_command(command, arguments, callback)
 	local stdout_data = {}
 	local stderr_data = {}
-
 	local stdout = vim.loop.new_pipe(false)
 	local stderr = vim.loop.new_pipe(false)
-
 	local process
-	process = vim.loop.spawn(cmd, {
-		args = args,
+	
+	process = vim.loop.spawn(command, {
+		arg = arguments or {},
 		stdio = { nil, stdout, stderr },
 	}, function(exit_code, signal)
 		stdout:close()
 		stderr:close()
 		process:close()
-
+		
 		local result = (exit_code == 0)
 		callback(result, {
 			exit_code = exit_code,
@@ -31,20 +26,25 @@ function M.check_connection(callback)
 			stderr = table.concat(stderr_data),
 		})
 	end)
-
+	
 	stdout:read_start(function(err, data)
 		if err then
 		elseif data then
 			table.insert(stdout_data, data)
 		end
 	end)
-
+	
 	stderr:read_start(function(err, data)
 		if err then
 		elseif data then
 			table.insert(stderr_data, data)
 		end
 	end)
+end
+
+function M.upload_file(command, arguments, callback)
+  print(command, vim.inspect(arguments))
+	-- M.execute_command(command, arguments, callback)
 end
 
 return M
--- lua/sftp/sftp.lua
+++ lua/sftp/sftp.lua
@@ -5,33 +5,79 @@ local cli = require("sftp.cli")
 local M = {}
 
 local plugin_opts = {}
-local connected = false
+local project_opts = {}
+local sftp_project = false
+
+local upload_file_autocmd = nil
 
 function M.setup(opts)
 	plugin_opts = opts.projects or {}
 end
 
+local function upload_file()
+	if not sftp_project then
+		return
+	end
+
+	local file_path = vim.api.nvim_buf_get_name(0)
+	if not file_path then
+		return
+	end
+
+	local file_name = vim.fn.fnamemodify(file_path, ":t")
+	if not file_name then
+		return
+	end
+
+	local remote_file_destination = string.gsub(file_path, "^" .. project_opts.local_path .. "/?", "")
+	local remote_path = project_opts.host .. ":" .. project_opts.remote_path .. "/" .. remote_file_destination
+
+	-- print("rsync -avz " .. file_path .. " " .. remote_path)
+
+	cli.upload_file("rsync", { "-avz", file_path, remote_path }, function(success, info)
+	   print(vim.inspect(info))
+		if not success then
+			print("Error")
+		else
+			print("Success")
+		end
+	end)
+
+	-- error code 23 = file not found
+end
+
+local function create_autocmd()
+	if not upload_file_autocmd then
+		upload_file_autocmd = vim.api.nvim_create_autocmd("BufWritePost", {
+			group = group,
+			callback = function()
+				upload_file()
+			end,
+		})
+	end
+end
+
+local function clear_autocmd()
+	if upload_file_autocmd then
+		vim.api.nvim_del_autocmd(upload_file_autocmd)
+	end
+end
+
 function M.check_project()
-	connected = false
 	local current_path = vim.fn.getcwd()
 
-	for key, project in pairs(plugin_opts) do
+	for _, project in pairs(plugin_opts) do
 		if project.local_path == current_path and project.host ~= nil and project.remote_path ~= nil then
-
-			cli.check_connection(function(success, details)
-        print(vim.inspect(details))
-        if not success then
-          print("Error connecting to server.")
-        else
-          print("ok")
-        end
-
-        -- error code 23 = file not found
-
-        -- check if actuall output of command means connection with sftp server is made
-			end)
+			project_opts = project
+			sftp_project = true
+			create_autocmd()
+			return
 		end
 	end
+
+	project_opts = {}
+	sftp_project = false
+	clear_autocmd()
 end
 
 return M
